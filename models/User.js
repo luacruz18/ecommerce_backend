9765a19b10ccedb66624309c8911e70cd0b8da7a
sncozzi <santiago.cozzi@anima.edu.uy>
2024-04-09 19:12:19 -0300

    Setup and User Model

---
+++ models/User.js
@@ -0,0 +1,46 @@
+const { Model, DataTypes } = require("sequelize");
+
+class User extends Model {
+  static initModel(sequelize) {
+    User.init(
+      {
+        id: {
+          type: DataTypes.BIGINT,
+          primaryKey: true,
+          autoIncrement: true,
+        },
+        name: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+        lastname: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+        email: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+        address: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+        phoneNumber: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+        password: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+      },
+      {
+        sequelize,
+        modelName: "User",
+      }
+    );
+    return User;
+  }
+}
+
+module.exports = User;
--- models/index.js
+++ models/index.js
@@ -1,49 +1,21 @@
 const { Sequelize, Model, DataTypes } = require("sequelize");
 
-class User extends Model {
-  static initModel(sequelize) {
-    User.init(
-      {
-        id: {
-          type: DataTypes.BIGINT, // Notar que si bien esto en BD se guarda como BIGINT (8 bytes), Sequelize lo retorna como String, ya que JavaScript no lo puede representar.
-          primaryKey: true,
-          autoIncrement: true,
-        },
-        firstname: {
-          type: DataTypes.STRING,
-          allowNull: false,
-        },
-        lastname: {
-          type: DataTypes.STRING,
-          allowNull: false,
-        },
-        email: {
-          type: DataTypes.STRING,
-          allowNull: false,
-          unique: true,
-        },
-        password: {
-          type: DataTypes.STRING,
-          allowNull: false,
-        },
-      },
-      {
-        sequelize,
-        modelName: "user",
-      }
-    );
-    return User;
-  }
-}
+const User = require("./User");
 
-const sequelize = new Sequelize("example_db", "root", "root", {
-  host: "127.0.0.1",
-  port: 3306,
-  dialect: "mysql",
-  logging: false,
-});
+const sequelize = new Sequelize(
+  process.env.DB_DATABASE,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_CONNECTION,
+    logging: false,
+  }
+);
 
 User.initModel(sequelize);
+Article.initModel(sequelize);
 
 module.exports = {
   sequelize,
