b7ffb6c4d0ea98af65910f465c21722a6c39f829
sncozzi <santiago.cozzi@anima.edu.uy>
2024-04-11 20:04:21 -0300

    Controller User (update)

--- controllers/userController.js
+++ controllers/userController.js
@@ -38,13 +38,33 @@ const userController = {
   update: async (req, res) => {
     try {
       const user = await User.findByPk(req.params.id);
-      await user.update(req.body);
+      const datosActualizables = {};
+      if (req.body.firstname) {
+        datosActualizables.firstname = req.body.firstname;
+      }
+      if (req.body.lastname) {
+        datosActualizables.lastname = req.body.lastname;
+      }
+      if (req.body.email) {
+        datosActualizables.email = req.body.email;
+      }
+      if (req.body.address) {
+        datosActualizables.address = req.body.address;
+      }
+      if (req.body.phoneNumber) {
+        datosActualizables.phoneNumber = req.body.phoneNumber;
+      }
+      if (req.body.password) {
+        datosActualizables.password = req.body.password;
+      }
+      await user.update(datosActualizables);
       return res.json(user);
     } catch (err) {
       console.log(err);
       return res.json({ message: "Oops! Something went wrong" });
     }
   },
+
   destroy: async (req, res) => {
     try {
       const user = await User.findByPk(req.params.id);
@@ -58,3 +78,4 @@ const userController = {
 };
 
 module.exports = userController;
+
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,31 +2,5 @@ const express = require("express");
 const router = express.Router();
 const { User } = require("../models");
 
-router.get("/", async (req, res) => {
-  const users = await User.findAll();
-  console.log(users);
-  return res.send(users);
-});
-
-router.get("/:id", async (req, res) => {
-  const { id } = req.params;
-  const user = await User.findByPk(id);
-  return res.json(user);
-});
-
-router.post("/", async (req, res) => {
-  const { firstname, lastname, email, address, phoneNumber, password } =
-    req.body;
-  console.log(req.body);
-  await User.create({
-    firstname,
-    lastname,
-    email,
-    address,
-    phoneNumber,
-    password,
-  });
-  res.send("Usuario creado correctamente");
-});
 
 module.exports = router;
