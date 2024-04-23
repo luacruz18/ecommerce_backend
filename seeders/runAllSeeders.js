69bb425358e07e2589c3b0740ed689a82c948b0c
sncozzi <santiago.cozzi@anima.edu.uy>
2024-04-23 20:29:11 -0300

    update runAllSeeders.js

--- seeders/runAllSeeders.js
+++ seeders/runAllSeeders.js
@@ -1,15 +1,21 @@
 require("dotenv").config();
 
 const userSeeder = require("./userSeeders");
-userSeeder();
+
 const categorySeeder = require("./categorySeeders");
-categorySeeder();
+
 const adminSeeder = require("./adminSeeders");
-adminSeeder();
+
 const orderSeeder = require("./orderSeeders");
-orderSeeder();
+
 const productSeeder = require("./productSeeders");
-productSeeder();
 
-
-console.log("All seeders were created")
+async function run() {
+  await userSeeder();
+  await categorySeeder();
+  await adminSeeder();
+  await orderSeeder();
+  await productSeeder();
+  console.log("All seeders were created");
+}
+run();
