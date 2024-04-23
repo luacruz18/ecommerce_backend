c2dff51aadcc0ec0529da6e6acc96f9182140678
Sofia Salaberry <sofia.salaberry9810@gmail.com>
2024-04-23 20:15:41 -0300

    update

--- seeders/categorySeeders.js
+++ seeders/categorySeeders.js
@@ -1,12 +1,8 @@
 const { Category } = require("../models/index");
 
 async function categorySeeder() {
-    const category = [
-        {name: "dulce"},
-        {name: "salado"},
-        {name: "agridulce"}
-     ]
-Category.bulkCreate(category);
-console.log("Category seeder is running")
+  const category = [{ name: "plain" }, { name: "toppings" }];
+  Category.bulkCreate(category);
+  console.log("Category seeder is running");
 }
-module.exports = categorySeeder; 
+module.exports = categorySeeder;
