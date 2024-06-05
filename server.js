4121ad70cd7553d5e0ef012bd38acf953c1d4294
bahiyyihteamo <luana.cruz@anima.edu.uy>
2024-06-05 18:37:56 -0300

    carousel

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 require("dotenv").config();
+const cors = require("cors");
 const express = require("express");
 const app = express();
 const routes = require("./routes");
 const port = 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
